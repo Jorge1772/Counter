0a5a32e3942ad4b928c2ceb06a01eba4ff442bef
Jorge <jorgesilva1892@gmail.com>
2021-02-09 17:33:58 -0500

    playing around with the useContext hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CounterHooks from './Counter'
 
 
+export const ThemeContext = React.createContext()
+
 
 function App() {
+  const [theme,setTheme] = useState('red')
   return (
-   <>
+
+    
+   <ThemeContext.Provider value={{backgroundColor: theme  }}>
    Counter
    <CounterHooks initialCount={0}/>
-   </>
+   <button onClick={() => setTheme(prevTheme => {
+     return prevTheme === 'red'? 'blue' : 'red'
+   }) }> Toggle</button>
+   </ThemeContext.Provider>
   );
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,15 @@
-import React ,{ useState }from 'react';
-
+import React ,{ useState, useContext  }from 'react';
+import { ThemeContext } from './App';
 
 export default function CounterHooks({initialCount}){
 const [count, setCount] = useState(initialCount)
+const style = useContext(ThemeContext)
     
     return(
     <div>
-        <button className='btn' onClick={()=> setCount(prevCount => -1 )}>-</button>
+        <button style={style} onClick={()=> setCount(prevCount => prevCount - 1 )}>-</button>
             <span>{count}</span>
-        <button className='btn' onClick={()=> setCount(prevCount => +1 )}>+</button>
+        <button style={style} onClick={()=> setCount(prevCount =>  prevCount + 1 )}>+</button>
     </div>
   )  
 }
